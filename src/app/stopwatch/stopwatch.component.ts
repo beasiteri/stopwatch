06c0999fe61688ff76505d29625c2d789756120f
beasiteri <siteribea@gmail.com>
2024-03-19 19:39:03 +0100

    Show the numbers on the screen

--- src/app/stopwatch/stopwatch.component.ts
+++ src/app/stopwatch/stopwatch.component.ts
@@ -1,15 +1,21 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-stopwatch',
   templateUrl: './stopwatch.component.html',
   styleUrls: ['./stopwatch.component.scss']
 })
-export class StopwatchComponent {
-  time = 0;
-
+export class StopwatchComponent implements OnInit {
   @HostBinding('style.display') display = 'flex';
   @HostBinding('style.justify-content') justifyContent = 'center';
+
+  time = 0;
+  numbers: string[] = [];
+  isMobile: boolean = window.innerWidth <= 600;
+
+  ngOnInit(): void {
+    this.calculateNumbers();
+  }
 
   calculateHourHandAngle(): number {
     const hours = (this.time / (1000 * 60 * 60)) % 12;
@@ -23,5 +29,17 @@ export class StopwatchComponent {
 
   calculateSecondHandAngle(): number {
     return (((this.time % 60000) / 60000) * 360) - 180;
+  }
+
+  calculateNumbers(): void {
+    const numberCount = 60;
+    const angleIncrement = 360 / numberCount;
+    let angle = -90;
+
+    for (let i = 0; i < numberCount; i++) {
+      const value = (i % 60 === 0 ? 60 : i % 60).toString().padStart(2, '0');
+      this.numbers.push(value);
+      angle += angleIncrement;
+    }
   }
 }
