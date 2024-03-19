4da56dae36f5b967d8fd8cdcacb31181403ef254
beasiteri <siteribea@gmail.com>
2024-03-19 19:16:54 +0100

    Style the stopwatch hands

--- src/app/stopwatch/stopwatch.component.ts
+++ src/app/stopwatch/stopwatch.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./stopwatch.component.scss']
 })
 export class StopwatchComponent {
+  calculateHourHandAngle() { }
 
+  calculateMinuteHandAngle() { }
+
+  calculateSecondHandAngle() { }
 }
