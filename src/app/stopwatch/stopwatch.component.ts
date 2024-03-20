edf80e55947d7069fd3ccde9bbe132fd90c2aaf3
beasiteri <siteribea@gmail.com>
2024-03-20 21:06:49 +0100

    Add lap to the timer

--- src/app/stopwatch/stopwatch.component.ts
+++ src/app/stopwatch/stopwatch.component.ts
@@ -72,4 +72,28 @@ export class StopwatchComponent implements OnInit {
     this.isRunning = false;
     this.laps = [];
   }
+
+  lap(): void {
+    this.laps.push(this.time);
+  }
+
+  formatTime(milliseconds: number): string {
+    const totalMilliseconds = milliseconds;
+    const millisecondsPart = totalMilliseconds % 1000;
+    const totalSeconds = Math.floor(totalMilliseconds / 1000);
+    const seconds = totalSeconds % 60;
+    const totalMinutes = Math.floor(totalSeconds / 60);
+    const minutes = totalMinutes % 60;
+    const hours = Math.floor(totalMinutes / 60);
+
+    return `${this.pad(hours)}:${this.pad(minutes)}:${this.pad(seconds)}.${this.padMilliseconds(millisecondsPart)}`;
+  }
+
+  pad(num: number): string {
+    return num < 10 ? '0' + num : num.toString();
+  }
+
+  padMilliseconds(num: number): string {
+    return num < 10 ? '00' + num : num < 100 ? '0' + num : num.toString();
+  }
 }
